f004051bc5fa3b80d9c1615793eef1da06269e48
bielcs12 <78964647+bielcs12@users.noreply.github.com>
2021-04-01 21:42:21 -0300

    Update AC1.ino

--- AC1.ino
+++ AC1.ino
@@ -4,3 +4,9 @@ const int verde = 6;
 const int azul = 7;
 
 bool estadoLedVermelho = false;
+//adiçao de um botao 
+const int botao1 = 2;
+const int botao2 = 3;
+unsigned long lastDebounceTime1 = 0;
+unsigned long lastDebounceTime2 = 0;
+const int botaoDelay = 100;
