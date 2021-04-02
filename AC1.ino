85577cf5d2821d3a1d3fa70329605c4e934716ad
mateodavalos <78964634+mateodavalos@users.noreply.github.com>
2021-04-01 21:26:00 -0300

    Update AC1.ino

--- AC1.ino
+++ AC1.ino
@@ -1,10 +1,9 @@
-[21:02, 01/04/2021] Cah: //variaveis da led
+//variaveis da led
 const int vermelho = 5;
 const int verde = 6;
 const int azul = 7;
-
 bool estadoLedVermelho = false;
-[21:03, 01/04/2021] Cah: //adiçao de um botao 
+//adiçao de um botao 
 const int botao1 = 2;
 const int botao2 = 3;
 unsigned long lastDebounceTime1 = 0;
